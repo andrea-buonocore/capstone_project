a7505e8ac29ebe1f5ba347a356ac062325121cc5
andrea-buonocore <andreabuonocore00@gmail.com>
2023-05-08 16:25:33 +0200

    aggiungto fade-in slogan

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
     return (
-        <div id="hero" className="p-3 p-md-5 d-flex align-items-center justify-content-center">
+        <div id="hero" className="p-3 p-md-5 d-flex align-items-center">
             <h1>Style Made Simple.</h1>
         </div>
     )
