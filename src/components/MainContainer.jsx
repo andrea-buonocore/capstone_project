15f61fec5290291ec8b0cbc2fff27e063e861029
andrea-buonocore <andreabuonocore00@gmail.com>
2023-05-08 17:23:56 +0200

    prime modifiche alle categorie

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,13 +1,23 @@
-import { Container } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import Hero from "./Hero";
 
 const MainContainer = () => {
     return (
         <>
-        <Hero/>
-        <Container>
-            Container
-        </Container>
+            <Hero />
+            <Container>
+                <Row className="justify-content-between">
+                    <Col xs={12} md={4} className="py-5 col_category d-flex align-items-center justify-content-center">
+                        <span className="fw-light">WOMEN</span>
+                    </Col>
+                    <Col xs={12} md={4} className="py-5 col_category d-flex align-items-center justify-content-center">
+                        <span className="fw-light">MEN</span>
+                    </Col>
+                    <Col xs={12} md={4} className="py-5 col_category d-flex align-items-center justify-content-center">
+                        <span className="fw-light">ACCESSORIES</span>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
