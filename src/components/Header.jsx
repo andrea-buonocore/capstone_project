c52f149d0cec1143b258a413d332d385ffd7a440
andrea-buonocore <andreabuonocore00@gmail.com>
2023-05-08 15:56:00 +0200

    prime modifiche all'header. cerco di implementare l'inputField a comparsa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import { Container } from "react-bootstrap";
 
 const Header = () => {
+
+    //riferimento al input field
+    let inputField = document.getElementById('inputField')
+    console.log(inputField);
+
+    const toggleInputField = () => {
+        inputField.classList.toggle('d-none');
+    }
+
     return (
-        <header className="py-5 fixed-top">
+        <header className="py-4 fixed-top">
             <Container className="d-flex align-items-center justify-content-between">
-                <span>StyleX</span>
+                <span className="fw-bold">STYLEX</span>
                 <div className="d-flex align-items-center">
-                    <span className="material-symbols-outlined mx-3">
+                    <input type="text" name="search" placeholder="search product..." id="inputField"/>
+                    <span className="material-symbols-outlined mx-3" onClick={toggleInputField}>
                         search
                     </span>
                     <span className="material-symbols-outlined mx-3">
